4d78a583d0065ec28191df739b06f2723cf7846d
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2024-03-24 22:33:17 +0530

    old changes

---
+++ Source/ProjectLight/Private/AI/LightAICharacter.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "AI/LightAICharacter.h"
+
---
+++ Source/ProjectLight/Public/AI/LightAICharacter.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/LightCharacterBase.h"
+#include "LightAICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLIGHT_API ALightAICharacter : public ALightCharacterBase
+{
+	GENERATED_BODY()
+	
+};
